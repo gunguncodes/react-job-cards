f8ad32421a021badea9fd196c23b558df5850982
gunguncodes <gungunkri865@gmail.com>
2026-07-07 11:00:09 +0530

    Made a login form with form validation

--- src/projects/LoginForm.jsx
+++ src/projects/LoginForm.jsx
@@ -16,11 +16,26 @@ function LoginForm() {
         });
     }
 
+    const [success, setSuccess] = useState("");
+
     function handleSubmit(event) {
         event.preventDefault();
-        console.log("Login Successful");
+        if(
+            user.name === "" ||
+            user.email === "" ||
+            user.password === "" 
+        ) {
+            setError("Please fill in all the fields.");
+            return;
+        }
+
+        setError("");
+        setSuccess("Login Successful");
+        console.log(user);
 
     }
+
+    const[error, setError] = useState("");
 
     return(
         <div className="min-h-screen bg-linear-to-br from-blue-200 via-purple-200 to-pink-200 flex justify-center items-center">
@@ -55,6 +70,22 @@ function LoginForm() {
                   onChange={handleChange}
                   className="w-full border border-gray-300 rounded-xl px-4 py-3 mb-4 outline-none focus:ring-2 focus:ring-purple-300"
                  />
+                 
+                 {
+                    error && (
+                        <p className="text-red-500 mb-4">
+                            {error}
+                        </p>
+                    )
+                 }
+
+                 {
+                    success && (
+                          <p className="text-green-500 mb-4">
+                              {success}
+                          </p>
+                    )
+                 }
 
                  <button
                  type="submit"
