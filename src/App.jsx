6ff9e5ae498730a007ee3b744a4708e4c433bbdc
gunguncodes <gungunkri865@gmail.com>
2026-07-10 13:40:18 +0530

    Learn React Router basics with navigation and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
-import GitHubProfileFinder from "./projects/GitHubProfileFinder";
+import { Routes, Route, NavLink } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <GitHubProfileFinder />  
+    return (
+        <>
+            <nav className="bg-purple-500 p-4 flex justify-center gap-8">
+
+                <NavLink
+                    to="/"
+                    className={({isActive})=>
+                    isActive
+                      ? "text-yellow-300 font-bold"
+                      : "text-white"
+                    }
+                >
+                    Home
+                </NavLink>
+
+                <NavLink
+                    to="/about"
+                    className={({isActive})=>
+                    isActive
+                      ? "text-yellow-300 font-bold"
+                      : "text-white"
+                    }
+                >
+                    About
+                </NavLink>
+
+                <NavLink
+                    to="/contact"
+                   className={({isActive})=>
+                    isActive
+                      ? "text-yellow-300 font-bold"
+                      : "text-white"
+                    }
+                >
+                    Contact
+                </NavLink>
+
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound/>} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
